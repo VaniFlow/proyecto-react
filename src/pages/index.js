139eb6d4a0d61461137d36498590603470c5ddd2
GuidoAquino <aquino.guido@hotmail.com>
2023-08-06 19:53:24 -0300

    Agreagando Cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
+import CardList from "@/components/organisms/CardList"
+import Cards from "@/components/organisms/Cards"
 import Footer from "@/components/organisms/Footer"
 
 
 export default function Home() {
   return (
     <>
-   
+    
+    <CardList />
     <Footer />
     </>
 
