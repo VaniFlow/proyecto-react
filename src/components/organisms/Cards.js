b68f0a857603ef91b47f54fa6cefbe1c635c7614
Alan Opresnik <alanopk328@gmail.com>
2023-08-08 02:58:30 +0200

    Añadido carousel para las cards y solucion a estilos de cards

--- src/components/organisms/Cards.js
+++ src/components/organisms/Cards.js
@@ -1,12 +1,12 @@
-
-
-
+import DirectionsCarFilledOutlinedIcon from '@mui/icons-material/DirectionsCarFilledOutlined';
+import FlightTakeoffOutlinedIcon from '@mui/icons-material/FlightTakeoffOutlined';
+import HotelOutlinedIcon from '@mui/icons-material/HotelOutlined';
 
 
 const Cards = ({card}) => {
   return (
     <>
-     <div className="relative w-80 h-[450px] rounded-[10px] overflow-hidden shadow-[0_15px_35px_rgba(0,0,0,0.25)] mb-[5px] text-left">
+     <div className="relative w-80 h-[450px] rounded-[10px] overflow-hidden  mb-[5px] text-left">
   <div className="opacity-70">
     <img
       src={card.imgfondo}
@@ -18,13 +18,13 @@ const Cards = ({card}) => {
   <div className="w-full z-[20] transition-[0.5s] p-5 left-0 -bottom-2.5">
     <img
       src={card.imglogo}
-      className="max-w-[100px] z-[300] relative"
+      className="max-w-[100px] max-h-[102px] z-[300] relative"
       alt="Logo"
     />
     <h3 className="text-[1.2em] font-bold text-white font-[bold] z-[321] relative opacity-100">
       {card.titulo}
     </h3>
-    <div className="relative">
+    <div className="relative h-[170px] ">
       <p className="text-white rounded ml-0 mr-[5px] my-0 p-[5px] font-bold">
         {card.textodescripcion}
       </p>
@@ -33,10 +33,13 @@ const Cards = ({card}) => {
       <h4 className="text-white mt-2.5 z-[329] text-[1.3em no-underline]">
         Comodidades
       </h4>
-      <ul className="relative flex gap-[5px] mt-2.5">
-        {/* Lista de íconos de comodidades */}
+      <ul className="relative flex gap-[5px] py-1 ">
+      <HotelOutlinedIcon/>
+      <FlightTakeoffOutlinedIcon/>
+      <DirectionsCarFilledOutlinedIcon/>
       </ul>
-      <div className="absolute bottom-0 left-0 w-full flex items-center justify-between">
+      <div className=' relative bottom-0'>
+      <div className=" bottom-0 left-0 w-full flex items-center justify-between">
         <a
           href="#"
           className="rounded-[10px] bg-customRed text-1em text-white text-decoration-none px-5 py-2 border-radius background-transition"
@@ -49,6 +52,7 @@ const Cards = ({card}) => {
         >
           ${card.precio}
         </a>
+        </div>
       </div>
     </div>
   </div>
