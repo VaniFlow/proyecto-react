335815f004787cd7aa17d44ef182fc9cbd7d8708
GuidoAquino <aquino.guido@hotmail.com>
2023-08-07 22:45:10 -0300

    subiendo nav y home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import CardList from "@/components/organisms/CardList"
 
 import Footer from "@/components/organisms/Footer"
+import Homepart from "@/components/organisms/Homepart"
 
 
 export default function Home() {
   return (
     <>
-    
+    <Homepart/>
     <CardList />
     <Footer />
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     screens: {
@@ -28,7 +29,8 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms')
-       ,
+    require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
+       
   ],
 }
