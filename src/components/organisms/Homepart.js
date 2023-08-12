f2c59de7fbb070bcb3a7abcccc87ea7777b8ffaa
Alan opresnik <alanopk328@gmail.com>
2023-08-12 20:29:34 -0300

    agregue un modal para eliminar la ultim cantidad de un carrito

--- src/components/organisms/Homepart.js
+++ src/components/organisms/Homepart.js
@@ -1,24 +1,33 @@
 import React from "react";
 import Nav from "../atoms/Nav";
 
-
 const Homepart = () => {
   return (
     <>
-<section id="inicio" className='h-[100vh] '>
-    <div className='h-full leading-[1] w-full absolute flex items-center justify-center flex-col my-auto text-center z-10  p-2'>
-    <h1 className='text-[80px] [text-shadow:1px_1px_2px_var(--tw-shadow-color)] shadow-black' style={{fontFamily:"letra-home"}}>Gold Sands Travel</h1>
+      <section
+        id="inicio"
+        className="h-[100vh] relative "
+      >
+        <div className="h-full leading-[1] w-full absolute flex items-center justify-center flex-col my-auto text-center   p-2">
+          <h1
+            className="text-[80px] [text-shadow:1px_1px_2px_var(--tw-shadow-color)] shadow-black"
+            style={{ fontFamily: "letra-home" }}
+          >
+            Gold Sands Travel
+          </h1>
 
-      <h2 className='text-[40px] [text-shadow:1px_1px_2px_var(--tw-shadow-color)] shadow-black'>La aventura de viajar</h2>
-    </div>
-    <img className="w-full h-full object-cover" src="https://hips.hearstapps.com/hmg-prod/images/pink-beach-komodo-island-royalty-free-image-993367800-1561711107.jpg" alt="" />   
-
-
-
-
-    </section>
+          <h2 className="text-[40px] [text-shadow:1px_1px_2px_var(--tw-shadow-color)] shadow-black">
+            La aventura de viajar
+          </h2>
+        </div>
+        <img
+          className="w-full h-full object-cover"
+          src="https://hips.hearstapps.com/hmg-prod/images/pink-beach-komodo-island-royalty-free-image-993367800-1561711107.jpg"
+          alt=""
+        />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Homepart
+export default Homepart;
--- src/components/organisms/cartItem.js
+++ src/components/organisms/cartItem.js
@@ -1,10 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { DataContext } from "../context/dataContext"; // Asegúrate de importar el contexto correctamente
+import Modal from "react-modal"; // Importa el componente Modal
 
 const CartItem = ({ cart, total }) => {
   const { setCart } = useContext(DataContext);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState(null);
 
   const handleIncrement = (itemId) => {
     const updatedCart = cart.map((item) =>
@@ -13,13 +16,61 @@ const CartItem = ({ cart, total }) => {
     setCart(updatedCart);
   };
 
+  const openModal = (itemId) => {
+    setItemToDelete(itemId);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setItemToDelete(null);
+  };
+
   const handleDecrement = (itemId) => {
-    const updatedCart = cart
-      .map((item) =>
-        item.id === itemId ? { ...item, quantity: item.quantity - 1 } : item
-      )
-      .filter((item) => item.quantity > 0);
-    setCart(updatedCart);
+  const itemToDelete = cart.find((item) => item.id === itemId);
+
+  if (itemToDelete && itemToDelete.quantity === 1) {
+    openModal(itemId);
+  } else {
+    decrementItem(itemId);
+  }
+};
+
+const decrementItem = (itemId) => {
+  const updatedCart = cart.map((item) =>
+    item.id === itemId
+      ? { ...item, quantity: item.quantity - 1 }
+      : item
+  )
+  setCart(updatedCart);
+};
+
+const handleDeleteConfirmed = () => {
+  const updatedCart = cart.filter((item) =>
+     itemToDelete.quantity > 0
+  );
+  console.log(updatedCart)
+  setCart(updatedCart);
+  closeModal();
+};
+  
+  
+  
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      textAlign: "center",
+      color: "black",
+      width: "600px",
+      height: "200px",
+      zIndex: 99999,
+      marginTop: "2rem",
+    },
   };
 
   return (
@@ -34,24 +85,62 @@ const CartItem = ({ cart, total }) => {
       <h4 className="text-black">Carrito</h4>
 
       {cart.map((card) => (
-        <div key={card.id} className="text-black flex items-center mb-6 gap-4">
-          <img
-            className="w-20 h-20 rounded"
-            src={card.imgfondo}
-            alt={card.titulo}
-          />
-          <p className="text-black w-[150px]">{card.titulo}</p>
-          <button onClick={() => handleIncrement(card.id)}>
-            <AddCircleOutlineIcon />
-          </button>
-          
-          <span className="text-black">{card.quantity}</span>
-          <button onClick={() => handleDecrement(card.id)}>
-            <RemoveCircleOutlineIcon />
-          </button>
-          <span className="text-black">${card.precio * card.quantity}</span>
-        </div>
+        <>
+          <div
+            key={card.id}
+            className="text-black flex items-center mb-6 gap-4"
+          >
+            <img
+              className="w-20 h-20 rounded"
+              src={card.imgfondo}
+              alt={card.titulo}
+            />
+            <p className="text-black w-[150px]">{card.titulo}</p>
+            <button onClick={() => handleIncrement(card.id)}>
+              <AddCircleOutlineIcon />
+            </button>
+
+            <span className="text-black">{card.quantity}</span>
+            <button onClick={() => handleDecrement(card.id)}>
+              <RemoveCircleOutlineIcon />
+            </button>
+            <span className="text-black">${card.precio * card.quantity}</span>
+          </div>
+        </>
       ))}
+      <Modal
+        style={customStyles}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Eliminar artículo"
+      >
+        <div className="flex flex-col items-center justify-center h-full">
+          <h2 className="text-black">¿Eliminar este artículo?</h2>
+          <div className="flex gap-2 justify-center mt-5">
+            <button
+              style={{
+                backgroundColor: "#F99999",
+                padding: ".5rem ",
+                borderRadius: "10px",
+                color: "white",
+              }}
+              onClick={handleDeleteConfirmed}
+            >
+              Eliminar
+            </button>
+            <button
+              style={{
+                border: "1px solid #F99999",
+                padding: ".5rem ",
+                borderRadius: "10px",
+              }}
+              onClick={closeModal}
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </Modal>
       <div>
         <h3 className="text-black text-right">Total: {total}</h3>
       </div>
