49e43fa5b54695de32affff50628b6700b19d39a
Alan Opresnik <alanopk328@gmail.com>
2023-08-14 03:22:41 +0200

    arregle problemas en el incremenart y decrementar del car

--- src/components/organisms/CardList.js
+++ src/components/organisms/CardList.js
@@ -1,14 +1,13 @@
-import { useEffect, useState, useContext  } from "react";
+import { useEffect, useState, useContext } from "react";
 import Slider from "react-slick";
 import Cards from "./Cards";
-
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { DataContext } from "../context/dataContext";
 
 const CardList = () => {
-  const Data = useContext(DataContext)
+  const Data = useContext(DataContext);
 
   const settings = {
     dots: true,
@@ -48,7 +47,7 @@ const CardList = () => {
           centerMode: true,
         },
       },
-    
+
       {
         breakpoint: 468,
         settings: {
@@ -59,33 +58,26 @@ const CardList = () => {
       {
         breakpoint: 550,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
     ],
   };
 
-
- 
-
- return (
-<section id="destinos">
-
-    <div className="py-20 px-2 bg-gradient-to-b from-[#03c3ec] to-red-200 font-sans">
-      <div className="text-center mb-6">
-        <h2 className="text-3xl font-semibold">Elige tu destino favorito</h2>
+  return (
+    <section id="destinos">
+      <div className="py-20 px-2 bg-gradient-to-b from-[#03c3ec] to-red-200 font-sans">
+        <div className="text-center mb-6">
+          <h2 className="text-3xl font-semibold">Elige tu destino favorito</h2>
+        </div>
+        <Slider {...settings}>
+          {Data.data.map((card) => (
+            <Cards key={card.id} card={card} />
+          ))}
+        </Slider>
       </div>
-      <Slider {...settings}>
-        {Data.data.map((card) => (
-          <Cards key={card.id} card={card} />
-        ))}
-      </Slider>
-    </div>
     </section>
-
- )
-
-
-}
+  );
+};
 export default CardList;
